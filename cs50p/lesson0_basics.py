cf0923ed8b9598c0790adffc9ec5f6220f0cb16d
itzbcb4 <hoanghaanh43@gmail.com>
2026-03-08 10:35:25 +0700

    Update lesson0_basics.py

--- cs50p/lesson0_basics.py
+++ cs50p/lesson0_basics.py
@@ -1,8 +1,14 @@
 def main():
     name = input("What's your name?")
     greet(name)
+    price = float(input("What's the price of the item? "))
+    print(f"The total price with tax is: {format_total(price)}")
 
 def greet(x):
   print("Hello,",x)
+
+def format_total(price, tax_rate=0.07):
+  total = price + (price * tax_rate)
+  return total
 
 main()
